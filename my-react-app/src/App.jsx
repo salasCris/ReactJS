29ec0b675bb0aa46fab532977a4fc8180ca3ee90
Cristian Salas Ventura <240186@utxicotepec.edu.mx>
2026-01-22 14:20:34 -0600

    add: se agrego a la preactica listas y funciones de usuarios

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,9 +7,67 @@ function App() {
       <h4>Alumnos</h4>
       <h4>Cristian</h4>
       <h4>Salas</h4>
-      <h4>Ventura</h4>
+
+      <UserComponent/>
+      <ProfileComponent/>
+      <FeedComponent/>
     </div>
   );
+}
+
+function UserComponent(){
+  const nombre = 'Cxxs';
+  const apellidos = 'Sxlxs Vxntxrx'
+  const nombrecompleto = <h3>El nombre es {nombre} y sus apellidos son {apellidos}</h3>
+  return <h3>User Component {nombrecompleto}</h3>
+}
+
+function ProfileComponent(){
+   const users = [
+    { id: 1, name:'Cristian', role: 'Web Developer'},
+    { id: 2, name:'Diana', role: 'Web Designer'},
+    { id: 1, name:'Emanuel', role: 'Team Leader'},
+  ]
+  return (
+  <>
+    <p>Lista de usuarios del sistema</p>
+    <ul>
+      {
+      users.map(function(user,index){
+        return(
+          <li key={index}>{user.name} es un {user.role}</li>
+        )
+      })
+    }
+    </ul>
+  </>
+  );
+}
+
+function FeedComponent(){
+  const users = [
+    { id: 1, material:'cemento', role: 'paredes'},
+    { id: 2, material:'barilla', role: 'la loza'},
+    { id: 1, material:'clavos', role: 'simbra'},
+    { id: 1, material:'martillo', role: 'clavos'},
+    { id: 1, material:'arena', role: 'mezcla'},
+  ]
+  
+  return (
+    <>
+    <p>Lista de usuarios del sistema</p>
+    <ul>
+      {
+      users.map(function(user,index){
+        return(
+          <li key={index}>{user.material} ese usa en {user.role}</li>
+        )
+      })
+    }
+    </ul>
+  </>
+  );
+
 }
 
 export default App;
